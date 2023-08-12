7376b39cdd76d3a25fd5dde542897a3039471afe
Hiroyuki Yagihashi <yo@yagipy.me>
2023-08-13 05:00:46 +0900

    Support benchmarker (#3)

--- local.tf
+++ local.tf
@@ -2,5 +2,7 @@ locals {
   app_name = "private-isu"
   region = "ap-northeast-1"
   ami = "ami-0676c829e30e00846" // https://github.com/catatsuy/private-isu
-  instance_type = "c5.large"
+  benchmark_ami = "ami-0582a2a7fbe79a30d" // https://github.com/catatsuy/private-isu
+  instance_type = "c6i.large"
+  benchmark_instance_type = "c6i.xlarge"
 }
